eebd54ae0824b3ff3283ed4591bc864c4be21795
Phuriq <85283698+Phuriq@users.noreply.github.com>
2023-02-28 00:37:18 +0700

    NEWWWW WW

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
-    <header>
-        <nav>
-            <ul class="nav_link">
-                <li><a href="">Home</a></li>
-                <li><a href="">Theater</a></li>
-                <li><a href="">Cinema</a></li>
-            </ul>
-        </nav>
-    </header>
+    <!-- <header>
+        <div class="card">
+            <div class="flex align-content-start flex-wrap card-container blue-container" style="min-height: 200px">
+                <div class="flex align-items-center justify-content-center bg-blue-500" style="min-width: 60px; min-height: 20px">
+                    <a class="font-bold text-white" href=""></a>
+                </div>
+                <div class="flex align-items-center justify-content-center bg-blue-500" style="min-width: 70px; min-height: 20px"><a class="font-bold text-white" href="">Home</a></div>
+                <div class="flex align-items-center justify-content-center bg-blue-500" style="min-width: 70px; min-height: 20px"><a class="font-bold text-white" href="">Theater</a></div>
+                <div class="flex align-items-center justify-content-center bg-blue-500" style="min-width: 70px; min-height: 50px"><a class="font-bold text-white" href="">Movie</a></div>
+            </div>
+        </div>
+    </header> -->
+    <div class="flex justify-content-between w-full bg-primary p-1">
+        <div>
+            <Avatar label="P" class="mx-3" style="background-color:#2196F3; color: #ffffff" shape="circle" />
+            <Button label="asd" />
+            <Button label="asd" />
+            <Button label="asd" />
+            <Button label="asd" />
+        </div>
+        <div class="flex align-content-center h-full">
+            <p class="mt-2"> User name </p>
+            <Avatar label="V" class="mx-3" style="background-color:#2196F3; color: #ffffff" shape="circle" />
+        </div>
+    </div>
 </template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+</style>
