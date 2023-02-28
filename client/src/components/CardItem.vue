51792eaa9e98af65771847c572b966e598fa8810
Phuriq <kiwwithfire123@gmail.com>
2023-02-28 18:17:24 +0700

    update

--- client/src/components/CardItem.vue
+++ client/src/components/CardItem.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <!-- <Card style="width: 20em; height: 25em">
         <div><img src="https://cdn.discordapp.com/attachments/404266538590535680/1079836229782208572/Eiga-Crayon-Shin-chan-Bakusui-Yumemi-World-Dai-Totsugeki.png" alt="Image"></div>
@@ -18,24 +17,3 @@ export default{
 
 <style>
  </style>
-=======
-<template>
-    <!-- <Card style="width: 20em; height: 25em">
-        <div><img src="https://cdn.discordapp.com/attachments/404266538590535680/1079836229782208572/Eiga-Crayon-Shin-chan-Bakusui-Yumemi-World-Dai-Totsugeki.png" alt="Image"></div>
-    </Card> -->
-    <Card style="width: 20em; height: 25rem;">
-            <template #header>
-                <img src="https://www.iposters.co.uk/media/catalog/product/0/3/0304JA_3.jpg" style="height: 25rem" />
-            </template>
-        </Card>
-</template>
-
-<script>
-export default{
-
-};
-</script>
-
-<style>
- </style>
->>>>>>> 3cd83abeb1d712dfe7c2ea676cdae1f584a60749
