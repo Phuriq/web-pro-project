8268af6fe68c5b83d55e22ae4f7f18ab666c5bac
Phuriq <85283698+Phuriq@users.noreply.github.com>
2023-02-28 18:18:06 +0700

    update sigiin

--- client/src/views/Signin.vue
+++ client/src/views/Signin.vue
@@ -10,18 +10,18 @@ import CardItem from "../components/CardItem.vue"
     <div class="flex text-4xl justify-content-center mt-8">
         <h5>Sign in to Madooxxxx</h5>
     </div>
-    <div class="flex justify-content-center">
+    <div class="flex justify-content-center mt-5">
         <Card style="width: 25rem" class="surface-50 border-round-xs">
             <template #content>
                 <div class="flex flex-column">
                     <h5>Username</h5>
                     <InputText type="text" v-model="value1" />
-                    <h5>Passwordㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+                    <h5 class="mt-5">Passwordㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
                         <router-link to="/forgot">
                         <span class="text-pink-100">Forgot password?</span>
                     </router-link>
                     </h5>
-                    <InputText type="text" v-model="value1" />
+                    <InputText type="text " v-model="value1" />
                 </div>
                 <router-link to="/home">
                 <div class="flex flex-column">
