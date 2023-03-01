c45ca0e1826c4d057193d97ab7175e73fe23eb00
Phuriq <85283698+Phuriq@users.noreply.github.com>
2023-03-01 21:01:15 +0700

    done passing compnent

--- client/src/components/CardItem.vue
+++ client/src/components/CardItem.vue
@@ -1,19 +1,17 @@
+<script>
+export default {
+    props: [
+        'img_src',
+        'title'
+    ]
+}
+</script>
+
 <template>
-    <!-- <Card style="width: 20em; height: 25em">
-        <div><img src="https://cdn.discordapp.com/attachments/404266538590535680/1079836229782208572/Eiga-Crayon-Shin-chan-Bakusui-Yumemi-World-Dai-Totsugeki.png" alt="Image"></div>
-    </Card> -->
-    <Card>
+    <Card style="height: 20rem;">
         <template #header value="movie">
-            <img src="https://f.ptcdn.info/899/076/000/r9vgdpya6KVhthaumxD-o.jpg"  />
-            <p>Eunchae</p>
+            <img :src="img_src" style="width: 100%;"/>
+            <p>{{ title }}</p>
         </template>
     </Card>
 </template>
-
-<script>
-export default {
-
-};
-</script>
-
-<style></style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,24 +3,21 @@ import CardItem from "../components/CardItem.vue"
 export default {
     data() {
         return {
-            cars: [
+            movies: [
                 {
+                    title: "qwe",
                     name: 'https://i.ytimg.com/vi/hH86YEyhirg/maxresdefault.jpg',
-                    numVisible: 3,
-                    numScroll: 3
                 },
                 {
+                    title: "asd",
                     name: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/330809379_744958067143601_7209193836136214967_n.png?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGXl6IAO3I8zGvqsH4tGhkL8Pskp_0qREHw-ySn_SpEQaPqb9pmi9iVQiIUhPhi-V8ptvlc0lYuxZyVdOcgpqGg&_nc_ohc=348oVz2ERagAX8U4CB9&_nc_oc=AQlN2cNHumZff68D5G5xvT9lAH0b7mi6NBbEjJiEoig6tzGWDIPZocT0Q-4EtwydDmM&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfB6alg5qquJ5GiJ4EgL4ZC4YefzynhqzQ-M5vui4qSn2Q&oe=6400FD13',
-                    numVisible: 2,
-                    numScroll: 2
                 },
                 {
+                    title: "zxc",
                     name: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/329148540_737158544674907_4747732838217062707_n.png?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFaR8VqAsYP9tmuOu9p0-zKt1nxolCdQJq3WfGiUJ1AmmbFOnOYAonxotP5EmoBRBAVZQLUklvzvCIXufHm_f1C&_nc_ohc=Cxpl8OwXjz4AX8cJfNG&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfBvp9ZRbuBXkqMVN0lp_onZGJW8ZXNb1PWOknChSmgf0w&oe=6401917B',
-                    numVisible: 1,
-                    numScroll: 1
                 }
             ],
-            movies: [
+            props: ['title'][
                 {
                     poster: 'https://i.pinimg.com/736x/c6/45/a5/c645a552af84202ce4bef5d9d287a9ce.jpg',
                 },
@@ -45,10 +42,10 @@ export default {
 <template>
     <div class="nav">
     </div>
-    <Carousel :value="cars" :numVisible="1" :numScroll="1" :autoplayInterval="4000" class="flex justify-content-center">
-        <template #item="cars">
-            <div class="flex justify-content-center" style="height: 280px" v-for="car in cars">
-                <img :src="car.name" style="height: 550px">
+    <Carousel :value="movies" :numVisible="1" :numScroll="1" :autoplayInterval="4000" class="flex justify-content-center">
+        <template #item="movies">
+            <div class="flex justify-content-center" style="height: 280px" v-for="movie in movies">
+                <img :src="movie.name" style="height: 550px">
             </div>
         </template>
     </Carousel>
@@ -63,8 +60,8 @@ export default {
     </div>
     <router-link to="/movieinfo">
         <div class="grid">
-            <div class="col-3" v-for="i in 10">
-                <CardItem></CardItem>
+            <div class="col-3" v-for="movie in movies">
+                <CardItem :title="movie.title" :img_src="movie.name"></CardItem>
             </div>
         </div>
     </router-link>
