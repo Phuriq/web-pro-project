0e9d312410dc7ce149ce1661a2d1b8e6d0b53577
Phuri Siripool <85283698+Phuriq@users.noreply.github.com>
2023-04-23 20:02:54 +0700

    ki
    jj

--- client/src/components/movieintheater.vue
+++ client/src/components/movieintheater.vue
@@ -16,14 +16,16 @@ export default {
 <template>
     <div id=card-bg class="flex flex-row surface-ground border-round-2xl mt-8" style="height: 250px; width: 750px ">
         <img :src="image" style="height: 250px" class="border-round-xl" />
-        <div class="flex flex-row mx-5">
-            <h4 class="text-xl mt-3"> {{ name }}</h4>
-        </div>
-        <div class="flex flex-row mx-5">
-            <h2 class="text-xl mt-3"> {{ time }} </h2>
-        </div>
-        <div class="flex flex-row mx-5">
-            <h3 class="text-xl mt-3"> {{ category }}</h3>
+        <div>
+            <div class="flex flex-row mx-5">
+                <h4 class="text-xl mt-3"> {{ name }}</h4>
+            </div>
+            <div class="flex flex-row mx-5">
+                <h2 class="text-xl mt-3"> {{ time }} </h2>
+            </div>
+            <div class="flex flex-row mx-5">
+                <h3 class="text-xl mt-3"> {{ category }}</h3>
+            </div>
         </div>
     </div>
 </template>
--- client/src/views/Movie-in-theter.vue
+++ client/src/views/Movie-in-theter.vue
@@ -36,7 +36,6 @@ export default {
     <div class="fadeinleft animation-duration-200">
         <div class="flex justify-content-center mt-5 mb-5">
             <Button class="p-button-text mr-5 text-3xl" label="NowShowing" />
-            <Button class="p-button-text text-3xl" label="Coming Soon" />
         </div>
         <div class="" id=bg3>
             <div class="row" v-for="movie in movies" :key="movie.id" id=bg1>
